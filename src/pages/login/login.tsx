36b87e7b488fa1b18689b11981d1ba7fd403a858
Ksenia Lavrukhina <burkamera@gmail.com>
2024-07-11 14:36:12 +0300

    Рендерит страницу логина

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,28 @@
+import Header from "../../components/header/header";
+import LoginForm from "../../components/login-form/login-form";
+
+function LoginPage(): JSX.Element {
+  return (
+    <div className="page page--gray page--login">
+      <Header/>
+
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">Sign in</h1>
+            <LoginForm/>
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#">
+                <span>Amsterdam</span>
+              </a>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default LoginPage
