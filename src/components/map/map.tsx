00f3bd7c3f696ab9c37de0a8e6dda73bc2ea9d32
Ksenia Lavrukhina <burkamera@gmail.com>
2024-08-25 20:56:46 +0300

    Исправляет карту и звездочки рейтинга

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -30,18 +30,34 @@ const Map = memo(({ className, city, offers, activeOffer }: Map): JSX.Element =>
     }
   }, [city, map]);
 
+  // useEffect(() => {
+  // //   if (map && offers) {
+  //     offers.forEach((offer) => {
+  //       if (offer.location) {
+  //         leaflet.
+  //           marker({
+  //             lat: offer.location.latitude,
+  //             lng: offer.location.longitude,
+  //           }, {
+  //             icon: activeOffer && activeOffer.id === offer.id ? currentCustomIcon : defaultCustomIcon,
+  //           }).addTo(markerLayer.current);
+  //       }
+  //     });
+  //   }
+  // }, [activeOffer, map, offers]);
+
   useEffect(() => {
     if (map && offers) {
+      markerLayer.current.clearLayers();
       offers.forEach((offer) => {
-        if (offer.location) {
-          leaflet.
-            marker({
-              lat: offer.location.latitude,
-              lng: offer.location.longitude,
-            }, {
-              icon: activeOffer && activeOffer.id === offer.id ? currentCustomIcon : defaultCustomIcon,
-            }).addTo(markerLayer.current);
-        }
+        leaflet
+          .marker({
+            lat: offer.location.latitude,
+            lng: offer.location.longitude,
+          }, {
+            icon: offer.id === activeOffer?.id ? currentCustomIcon : defaultCustomIcon,
+          })
+          .addTo(markerLayer.current);
       });
     }
   }, [activeOffer, map, offers]);
