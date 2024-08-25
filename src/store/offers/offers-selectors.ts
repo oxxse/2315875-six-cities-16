9a2a530181dcbf89a4185f0d02760896e036fc8d
Ksenia Lavrukhina <burkamera@gmail.com>
2024-08-26 02:06:19 +0300

    Исправляет тип

--- src/store/offers/offers-selectors.ts
+++ src/store/offers/offers-selectors.ts
@@ -1,8 +1,7 @@
 import { State } from '../../types/state';
 import { NameSpace } from '../../const';
-import { PlaceCard } from '../../types/types';
 
-const selectOffers = (state: Pick<State, NameSpace.Offers>) : PlaceCard[] => state[NameSpace.Offers].offers;
+const selectOffers = (state: Pick<State, NameSpace.Offers>) => state[NameSpace.Offers].offers;
 const selectOffersDataLoading = (state: Pick<State, NameSpace.Offers>) => state[NameSpace.Offers].isOffersDataLoading;
 const selectFavoriteOffers = (state: Pick<State, NameSpace.Offers>) => state[NameSpace.Offers].favoriteOffers;
 const selectOfferDetails = (state: Pick<State, NameSpace.Offers>) => state[NameSpace.Offers].offerDetails;
